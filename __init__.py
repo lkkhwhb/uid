92c2bb10c5f6103a70b12e8d30f3b0eb632b869f
Bhargav <bhargavbprd@gmail.com>
2025-11-15 15:06:56 +0530

    Clean up comments in __init__.py
    Removed comments about C++ module import and error handling.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
-# Import the function from our compiled C++ module (_core)
 try:
     from ._core import _id
 except ImportError:
-    # This provides a helpful error if the C++ extension failed to compile
     def _id(k=12):
         raise ImportError("uid C++ extension not built. Please check your compiler setup and reinstall.")
 
-# This defines the public API of the package
 __all__ = ["_id"]
